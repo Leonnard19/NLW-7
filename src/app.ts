f7dada2dc00012b08d3f3bb9298357f3f8ba775f
Leonardo <56121325+Leonnard19@users.noreply.github.com>
2021-10-22 11:59:19 -0300

    NLW #7 - Aula 1

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,25 @@
 import "dotenv/config";
 import express from "express";
-
+import http from "http";
+import cors from "cors";
+import { Server } from "socket.io";
 import { routes } from "./routes";
 
 const app = express();
+app.use(cors());
+
+const serverHttp = http.createServer(app);
+
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`Usuário conectado no socket ${socket.id}`);
+});
+
 app.use(express.json());
 
 app.use(routes);
@@ -20,6 +36,4 @@ app.get("/signin/callback", (request, response) => {
   return response.json(code);
 });
 
-app.listen(3333, () => {
-  console.log("Server is running on port 3333");
-});
+export { serverHttp, io };
